8bc13b91757d57229455d5ab734bc7ed9325238f
Drawlone <1846405414@qq.com>
2018-11-10 18:18:35 +0800

    能够发射子弹了，砰砰砰！

--- settings.py
+++ settings.py
@@ -11,4 +11,11 @@ class Settings:
         self.screen_width = 900
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        # 飞船速度
         self.ship_speed_factor = 1.5
+        # 子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
